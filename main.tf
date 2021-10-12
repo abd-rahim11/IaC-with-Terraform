d686393098eb783207c71c8d98b2637319af6f2e
r7ouma <r7ouma@example.com>
2021-10-12 22:32:21 +0100

    create subnet resource

--- main.tf
+++ main.tf
@@ -3,8 +3,12 @@ provider "aws" {
     shared_credentials_file = "/home/abderrahim/.aws/credentials"
 }
 
+
 variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip" {}
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -13,3 +17,13 @@ resource "aws_vpc" "myapp-vpc" {
         "Name" = "${var.env_prefix}-vpc"
     }
 } 
+
+resource "aws_subnet" "myapp-subnet-1" {
+   vpc_id = aws_vpc.myapp-vpc.id
+   cidr_block = var.subnet_cidr_block   
+   availability_zone = var.avail_zone
+   tags = {
+        "Name" = "${var.env_prefix}-subnet-1"
+   }    
+} 
+
