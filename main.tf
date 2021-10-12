87b54d3b4dbe90e4a6b73f1a0294783af330186f
r7ouma <r7ouma@example.com>
2021-10-12 22:24:13 +0100

    initialize terraform

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ provider "aws" {
     region = "us-east-1"
     shared_credentials_file = "/home/abderrahim/.aws/credentials"
 }
+
+variable "vpc_cidr_block" {}
+variable "env_prefix" {}
+
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        "Name" = "${var.env_prefix}-vpc"
+    }
+} 
