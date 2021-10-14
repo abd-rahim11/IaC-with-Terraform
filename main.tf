20cd2fdaedfcd74d988f622ae61f64560d63d2ee
r7ouma <r7ouma@example.com>
2021-10-14 19:55:42 +0100

    create shell script for user_data field

--- main.tf
+++ main.tf
@@ -113,13 +113,7 @@ resource "aws_instance" "myapp-server" {
         "Name" = "${var.env_prefix}-server"
     }
 
-    user_data = <<EOF
-                    #!/bin/bash
-                    sudo yum update -y && sudo yum install -y docker
-                    sudo systemctl start docker 
-                    sudo usermod -aG docker ec2-user
-                    docker run -p 8080:80 nginx:latest                    
-                EOF
+    user_data = file("entry-script.sh")
 
 
 
