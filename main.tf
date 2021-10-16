5d45daf2b6cad4027aa5ff5ca8b9677d0811534e
r7ouma <r7ouma@example.com>
2021-10-16 14:00:28 +0100

    modularize tf project by adding webserver module

--- main.tf
+++ main.tf
@@ -23,97 +23,18 @@ module "myapp-subnet" {
 
 }
 
-
-resource "aws_default_security_group" "default-sg" {
+module "myapp-server" {
+    source = "./modules/webserver"
     vpc_id = aws_vpc.myapp-vpc.id
-
-    ingress { 
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip]
-    }
-
-    ingress { 
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] 
-    }
-
-    egress { 
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"] 
-        prefix_list_ids = []
-    }
-    tags = {
-      "Name" = "${var.env_prefix}-default-sg"
-    }
-}
-
-
-
-
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
-resource "aws_key_pair" "ssh-key" {
-    key_name = "server-key" 
-    public_key = file(var.public_key_location)
-
-}
-
-resource "aws_instance" "myapp-server" {
-    ami = data.aws_ami.latest-amazon-linux-image.id
-    instance_type = var.instance_type
-
     subnet_id = module.myapp-subnet.subnet.id
-    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-    availability_zone = var.avail_zone
-    associate_public_ip_address = true
-    key_name = aws_key_pair.ssh-key.key_name
-    tags = {
-        "Name" = "${var.env_prefix}-server"
-    }
-    /*
-    connection {
-      type = "ssh"
-      host = self.public_ip
-      user = "ec2-user"
-      private_key = file(var.private_key_location)
-    }
-
-    provisioner "file" {
-        source = "entry-script.sh"
-        destination = "/home/ec2-usr/entry-script-on-ec2.sh"
-    }
-    provisioner "remote-exec" {
-      script = file("entry-script-on-ec2.sh")
-    }
-    #user_data = file("entry-script.sh")
-
-    provisioner "local-exec" {
-        command = "echo ${self.public_ip} > output.txt"
-      
-    }
-    */
-
+    env_prefix = var.env_prefix
+    my_ip = var.my_ip
+    image_name = var.image_name
+    public_key_location = var.public_key_location
+    instance_type = var.instance_type
+    avail_zone = var.avail_zone
+    
 }
-
-
-
 
 
 
--- variables.tf
+++ variables.tf
@@ -4,5 +4,6 @@ variable "avail_zone" {}
 variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
+variable "image_name" {}
 variable "public_key_location" {}
-variable "private_key_location" {}
+#variable "private_key_location" {}
